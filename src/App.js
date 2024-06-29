9ce3a4e7ccb91ff3d9b90d01874b659c87ef7a00
ViraniVatsal <157736100+ViraniVatsal@users.noreply.github.com>
2024-06-29 14:40:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./Components/Home";
+import Header from "./Components/header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
